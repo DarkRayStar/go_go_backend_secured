e9fe239810856f64bb196eef36547a9f7dde0cac
inwick <indikasandun2015@gmail.com>
2023-10-03 16:41:21 +0530

    Created Environment variables and modified the gitignore file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 // const helmet = require('helmet');
 
@@ -26,47 +26,46 @@ const port = process.env.PORT || 5050;
 // };
 
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 100, 
-    message: 'Too many requests from this IP, please try again later.',
-  });
-  
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+  message: 'Too many requests from this IP, please try again later.',
+});
+
 app.use(limiter);
 // app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
-    'useNewUrlParser': true
-}
-);
+  useNewUrlParser: true,
+});
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+  console.log('MongoDB database connection established successfully');
+});
 
 //Customer Routes
 const cartRouter = require('./routes/customer-routes/cart');
 const favRouter = require('./routes/customer-routes/favItems');
-const reviewRouter = require('./routes/customer-routes/review')
+const reviewRouter = require('./routes/customer-routes/review');
 
 // user management routes
-const userRoutes = require("./routes/userManagement-routes/userRegistration");
-const userLoginRoutes = require("./routes/userManagement-routes/userLogin");
-const passwordResetRoutes = require("./routes/userManagement-routes/passwordReset");
+const userRoutes = require('./routes/userManagement-routes/userRegistration');
+const userLoginRoutes = require('./routes/userManagement-routes/userLogin');
+const passwordResetRoutes = require('./routes/userManagement-routes/passwordReset');
 
 //Store admin routes
-const storeAdmin = require("./routes/storeAdmin-routes/storeAdmin.routes")
+const storeAdmin = require('./routes/storeAdmin-routes/storeAdmin.routes');
 
 const req = require('express/lib/request');
 
 // Delivery Routes
-const deliveryRoutes = require("./routes/delivery-routes/deliveryOrder");
+const deliveryRoutes = require('./routes/delivery-routes/deliveryOrder');
 
 //Customer Routes
 app.use('/cart', cartRouter);
@@ -74,16 +73,16 @@ app.use('/favorites', favRouter);
 app.use('/reviews', reviewRouter);
 
 // User Management Routes
-app.use("/user", userRoutes);
-app.use("/user/login", userLoginRoutes);
-app.use("/user/password-reset", passwordResetRoutes);
+app.use('/user', userRoutes);
+app.use('/user/login', userLoginRoutes);
+app.use('/user/password-reset', passwordResetRoutes);
 
 // Delivery Routes
-app.use("/delivery", deliveryRoutes);
+app.use('/delivery', deliveryRoutes);
 
 //storeAdmin routes
-app.use("/storeAdmin", storeAdmin);
+app.use('/storeAdmin', storeAdmin);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
